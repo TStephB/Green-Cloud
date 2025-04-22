685893f848f31091d6d874b1843d8be2184ad3fb
Sette-Th <sette.toure@centrale-casablanca.ma>
2025-04-22 22:31:46 +0100

    commit du code dht22_reader.py

--- sensors/dht22_reader.py
+++ sensors/dht22_reader.py
@@ -1,1 +1,18 @@
+import Adafruit_DHT
+import time
+
+# Définir le modèle du capteur et le numéro du GPIO connecté
+capteur = Adafruit_DHT.DHT22
+gpio_pin = 4  # Remplacer par le GPIO connecté à ton capteur
+
+while True:
+    humidite, temperature = Adafruit_DHT.read_retry(capteur, gpio_pin)
+
+    if humidite is not None and temperature is not None:
+        print(f'Température : {temperature:.1f}°C, Humidité : {humidite:.1f}%')
+    else:
+        print('Échec de la lecture du capteur. Réessayer !')
+
+    # Attendre 2 secondes avant la prochaine lecture
+    time.sleep(2)
 
